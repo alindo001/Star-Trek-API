3b1d6cd8e635355521934c8e25aed0951438cc6f
Andres <andreslindo20@gmail.com>
2022-06-08 23:25:04 -0400

    express

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
-const cors = require(cors)
+const cors = require('cors')
+const { request } = require('http')
 const PORT = 8000
 
 app.use(cors())
+
+const aliens = {}
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get("/api/:alienName", (req, res) =>{
+    const alliensName = req.params.alienName.toLowerCase()
+    if( aliens[alliensName]){
+        res.json(aliens[alliensName])
+    } else {
+        res.json(aliens['humans'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, () =>{
+    console.log("Server is running")
+})
+
